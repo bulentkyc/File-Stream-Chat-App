1426dff674d4643065aad69cea4859e3eeb16ff5
bulentkyc <bulent.kayici@digitalcareerinstitute.org>
2019-06-18 14:24:16 +0200

    Chat conpleted as filebased

--- app.js
+++ app.js
@@ -23,15 +23,31 @@ const server = http.createServer((req, res)=>{
         
     }
     else if(req.url == '/setChat'){
+        //Get data from request.
         var body = "";
         req.on('data', function (chunk) {
             body += chunk;
         });
+        //Process data on back-end
         req.on('end', function () {
-            console.log('POSTed: ' + body);
+            //Read chat history from file
+            let lastChat;
+            const chat = fs.createReadStream(__dirname+'/data.txt', {encoding: 'utf8'});
+            chat.on('data', (buffer)=>{
+                lastChat = buffer;
+            
+            //Convert data to object and get parameters.
+            nick =  JSON.parse(body).nickName;
+            msg = JSON.parse(body).message;
+
+            //Write to the file new chat line with old lines.
+            const newChat = fs.createWriteStream(__dirname + '/data.txt');
+            newChat.write(`${lastChat}\n ${nick} : ${msg}\n`);
             res.writeHead(200);
             res.end('<html><body><p>Your Request has been received!</p></body></html>');
+            });
         });
+
     }
 });
 
